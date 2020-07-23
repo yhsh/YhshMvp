3db06d140422fce99dabe8245a68ff50330ecee8
xiayiye5 <13343401268@qq.com>
2020-07-23 22:29:50 +0800

    增加时间注释

--- app/src/main/java/com/xiayiye/yhshmvp/ui/mvp/presenter/PagePresenter.kt
+++ app/src/main/java/com/xiayiye/yhshmvp/ui/mvp/presenter/PagePresenter.kt
@@ -40,6 +40,7 @@ import com.xiayiye.yhshmvp.ui.mvp.view.PageView
  * 项目名称：yhshmvp
  * 文件包名：com.xiayiye.yhshmvp.ui.mvp.presenter
  * 文件说明：创建远程分支命名要合理
+ * 22点29分
  */
 class PagePresenter : BasePresenter<PageView>() {
     fun showData(msg: String) {
